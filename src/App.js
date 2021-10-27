5fa9d43fdde6d356661e92c35051c32f463d6eca
Justina Andrulionyte <69106997+voidbeary@users.noreply.github.com>
2021-10-27 16:29:17 +0300

    Create React App and add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header
+        className="w-100 py-2 ps-3 mb-4"
+        style={{ backgroundColor: "#502", color: "white" }}
+        id="bob"
+      >
+        <h1>To do list</h1>
       </header>
-    </div>
+      <main className="container pxsm-5">
+        <ul className="list-group"></ul>
+        <form>
+          <input
+            id="new-item"
+            className="form-control"
+            placeholder="New to do"
+            aria-label="new todo text"
+            autoFocus=""
+            tabIndex="0"
+            required
+          />
+          <label htmlFor="new-item">
+            <input
+              type="submit"
+              value="Add item"
+              className="btn btn-primary d-none"
+            ></input>
+          </label>
+        </form>
+      </main>
+    </>
   );
 }
 
